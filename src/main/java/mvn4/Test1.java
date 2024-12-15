775df4ca98c1295f17d5e31f3b37428d7e1613eb
ApurvKolekar <108112106+ApurvKolekar@users.noreply.github.com>
2024-12-15 12:07:59 +0530

    mainFirst
    mainFirst

---
+++ src/main/java/mvn4/Test1.java
@@ -0,0 +1,12 @@
+package mvn4;
+import org.testng.annotations.*;
+public class Test1 {
+	@Test
+	public static void test1() {
+System.out.println("Test1");
+	}
+	@Test
+	public static void test2() {
+System.out.println("Test12");
+	}
+}
